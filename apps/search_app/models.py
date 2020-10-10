caceb51dcd6e8090478f5465b2d1a5404d8e0497
kelli <kellikells@gmail.com>
2020-10-10 13:02:24 -0700

    created User Model Table

--- apps/search_app/models.py
+++ apps/search_app/models.py
@@ -6,18 +6,58 @@ import re
 # for validating an Email 
 regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
-
+# ------------------------------------------
 class User(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     gender = models.CharField(max_length = 255)
-    image_path = 
-    sport = 
+    image_path = models.TextField()
+    sport = models.CharField(max_length = 30)
 
     created_at = models.DateField(auto_now_add = True)
     updated_at = models.DateField(auto_now = True)
 
-
-
     def __str__(self):
-        return('first_name= ' + self.first_name)
+        return('first_name= ' + self.first_name + ', last_name= ' + self.last_name + ', gender= ' + self.gender + ', image_path= ' + self.image_path + ', sport= ' + self.sport)
+    
+
+# ------------------------------------------
+# class SPORT(models.Model):
+
+
+
+
+# ------------------------------------------
+
+
+
+
+# ------------------------------------------
+# class User(models.Model):
+#     BASKETBALL = 1
+#     VOLLEYBALL = 2
+#     BASEBALL = 3
+#     SOCCER = 4
+#     FOOTBALL = 5
+#     SPORT_CHOICES = (
+#         (BASKETBALL, 'Basketball'),
+#         (VOLLEYBALL, 'Volleyball'),
+#         (BASEBALL, 'Baseball'),
+#         (SOCCER, 'Soccer'),
+#         (FOOTBALL, 'Football'),
+#     )
+
+#     first_name = models.CharField(max_length = 255)
+#     last_name = models.CharField(max_length = 255)
+#     gender = models.CharField(max_length = 255)
+#     image_path = models.TextField()
+#     sport = models.IntegerField(
+#         choices = SPORT_CHOICES,
+#     )
+
+#     created_at = models.DateField(auto_now_add = True)
+#     updated_at = models.DateField(auto_now = True)
+
+#     def __str__(self):
+#         return('first_name= ' + self.first_name + ', last_name= ' + self.last_name + ', gender= ' + self.gender + ', image_path= ' + self.image_path)
+# ------------------------------------------
